d9c2fd2d6733dab11017b82b2b71a0da009816b6
abulhassan90 <abul.king1990@gmail.com>
2023-03-21 18:17:46 +0530

    Update sidebar layout design

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+(() => {
+  "use strict";
+
+  document
+    .querySelector("#navbarSideCollapse")
+    .addEventListener("click", () => {
+      document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
+})();
