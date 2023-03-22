b81681eddcde028bfb67a0a30d2ecdc281b745a7
abulhassan90 <abul.king1990@gmail.com>
2023-03-22 19:09:32 +0530

    intro Layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,20 @@
 
   document
     .querySelector("#navbarSideCollapse")
-    .addEventListener("click", () => {
+    ?.addEventListener("click", () => {
       document.querySelector(".offcanvas-collapse").classList.toggle("open");
+      document
+        .querySelector("#navbarSideCollapse .bi")
+        .classList.toggle("bi-x");
     });
+
+  document.querySelectorAll(".nav-item").forEach((link) => {
+    link.addEventListener("click", (e) => {
+      var current = document
+        .querySelector(".nav-pills")
+        .getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      e.currentTarget.className += " active";
+    });
+  });
 })();
