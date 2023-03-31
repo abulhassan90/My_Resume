447127b44dfc1bf64054ddcaf603ac8ed6ccf337
abulhassan90 <abul.king1990@gmail.com>
2023-03-31 18:57:07 +0530

    Contact Email Script

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -20,4 +20,53 @@
       false
     );
   });
+
+  let name = document.getElementById("inputName");
+  let mobile = document.getElementById("inputMobile");
+  let email = document.getElementById("inputEmail");
+  let subject = document.getElementById("inputSubject");
+  let message = document.getElementById("inputMessage");
+  let formMessage = document.querySelector(".form-message");
+
+  document.getElementById("contactForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (
+      name.value &&
+      isValidEmail(email.value) &&
+      subject.value &&
+      message.value
+    ) {
+      sendEmail();
+
+      formMessage.classList.add("show");
+      setTimeout(() => {
+        formMessage.classList.remove("show");
+      }, 2000);
+    }
+
+    return false;
+  });
+
+  function sendEmail() {
+    Email.send({
+      SecureToken: "9c45e8df-72ea-444c-aef3-3b5e72b3609a",
+      To: "abul.king1990@gmail.com",
+      From: "abuleditor2@gmail.com",
+      Subject: subject.value,
+      Body: ` 
+      <strong>Name:</strong> ${name.value}<br/> 
+      <strong>Mobile:</strong> ${mobile.value}<br/> 
+      <strong>Email:</strong> ${email.value}<br/> 
+      <strong>Subject:</strong> ${subject.value}<br/> 
+      <strong>Message:</strong> <br/>${message.value}`,
+    }).then((message) => console.log(message));
+  }
+
+  // Helper function to validate email address format
+  function isValidEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    return regex.test(email);
+  }
 })();
